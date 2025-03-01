c071c5d4464cd77cf6d4a169a6fef1b723acd091
FeelsBotMan <sjb0229@gmail.com>
2025-03-01 15:24:55 +0900

    feat(security): Helmet 미들웨어 추가하여 Express 앱 보안 강화

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { errorHandler } from './src/middlewares/errorHandler.js';
@@ -8,6 +9,7 @@ import usersRouter from './src/routes/users.js';
 
 
 const app = express();
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
