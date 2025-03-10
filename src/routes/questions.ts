3d63faa6a6452e61a065463f922cbdd8030a9915
Jade Kim <csia19030@gmail.com>
2025-03-10 15:01:19 +0900

    chore: add jwt authentication for questions api calls

--- src/routes/questions.ts
+++ src/routes/questions.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getQuestionDetail, getWeeklyQuestionDetail, getAllQuestions } from "../controllers/questionsController";
 import { validateGetQuestionDetail } from "../middlewares/questions.validator";
+import authMiddleware from "../middlewares/authMiddleware";
 
 const router = Router();
 
-router.get('/', getAllQuestions);
-router.get('/weekly', getWeeklyQuestionDetail);
-router.get('/:id', validateGetQuestionDetail, getQuestionDetail);
+router.get('/', authMiddleware.authenticate, getAllQuestions);
+router.get('/weekly', authMiddleware.authenticate, getWeeklyQuestionDetail);
+router.get('/:id', authMiddleware.authenticate, validateGetQuestionDetail, getQuestionDetail);
 
 export default router;
