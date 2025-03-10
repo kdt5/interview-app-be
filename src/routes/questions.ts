8bf928a79c8f1bf9f22cf46ce97c841013f56278
Jade Kim <csia19030@gmail.com>
2025-03-10 21:55:35 +0900

    fix: make stricter rules for question id validation and add validation for position query

--- src/routes/questions.ts
+++ src/routes/questions.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { getQuestionDetail, getWeeklyQuestionDetail, getAllQuestions } from "../controllers/questionsController";
-import { validateGetQuestionDetail } from "../middlewares/questionsValidator";
+import { validateGetAllQuestionQuery, validateGetQuestionDetail } from "../middlewares/questionsValidator";
 import authMiddleware from "../middlewares/authMiddleware";
 
 const router = Router();
 
-router.get('/', authMiddleware.authenticate, getAllQuestions);
+router.get('/', authMiddleware.authenticate, validateGetAllQuestionQuery, getAllQuestions);
 router.get('/weekly', authMiddleware.authenticate, getWeeklyQuestionDetail);
 router.get('/:id', authMiddleware.authenticate, validateGetQuestionDetail, getQuestionDetail);
 
