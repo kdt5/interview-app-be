31b79fe559360039e2ed914a0b7d9dc038be6a1e
Jade Kim <csia19030@gmail.com>
2025-03-27 20:48:33 +0900

    chore: change numbered status code in validationErrorMiddleware to StatusCodes library

--- src/middlewares/validationErrorMiddleware.ts
+++ src/middlewares/validationErrorMiddleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
 import { validationResult } from "express-validator";
+import { StatusCodes } from "http-status-codes";
 
 export const validationErrorMiddleware: RequestHandler = (
   req: Request,
@@ -8,7 +9,7 @@ export const validationErrorMiddleware: RequestHandler = (
 ): void => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
+    res.status(StatusCodes.BAD_REQUEST).json({ errors: errors.array() });
     return;
   }
 
