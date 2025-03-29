65c35aa7e55a03b1b8f1b1a92de2a3fbad3569a6
FeelsBotMan <sjb0229@gmail.com>
2025-03-29 15:21:13 +0900

    Position 테이블에 새로운 직무 추가 및 Category 모델에 position_id 필드에 대한 인덱스 생성

--- prisma/migrations/20250329053056_position_table/migration.sql
+++ prisma/migrations/20250329053056_position_table/migration.sql
@@ -23,7 +23,9 @@ CREATE TABLE `Position` (
 INSERT INTO `Position` (`id`, `code`, `name`, `description`, `order`, `created_at`, `updated_at`)
 VALUES 
   (1, 'BACKEND_DEVELOPER', '백엔드 개발자', '서버 개발을 담당하는 개발자입니다.', 1, NOW(), NOW()),
-  (2, 'FRONTEND_DEVELOPER', '프론트엔드 개발자', '웹 클라이언트 개발을 담당하는 개발자입니다.', 2, NOW(), NOW());
+  (2, 'FRONTEND_DEVELOPER', '프론트엔드 개발자', '웹 클라이언트 개발을 담당하는 개발자입니다.', 2, NOW(), NOW()),
+  (3, 'FULLSTACK_DEVELOPER', '풀스택 개발자', '프론트엔드와 백엔드 개발을 모두 담당하는 개발자입니다.', 3, NOW(), NOW()),
+  (4, 'UXUI_DESIGNER', 'UXUI 디자이너', '사용자 경험과 인터페이스를 디자인하는 디자이너입니다.', 4, NOW(), NOW());
 
 -- Category 테이블에 position_id 컬럼 추가
 ALTER TABLE `Category` ADD COLUMN `position_id` INTEGER NULL;
@@ -44,3 +46,6 @@ ALTER TABLE `User` ADD COLUMN `position_id` INTEGER NULL;
 -- AddForeignKey
 ALTER TABLE `User` ADD CONSTRAINT `User_position_id_fkey` FOREIGN KEY (`position_id`) REFERENCES `Position`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 ALTER TABLE `Category` ADD CONSTRAINT `Category_position_id_fkey` FOREIGN KEY (`position_id`) REFERENCES `Position`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddIndex
+CREATE INDEX `Category_position_id_idx` ON `Category`(`position_id`);
