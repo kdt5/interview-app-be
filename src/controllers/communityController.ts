78af7a176295c99ea5ed9984929551b95faac8bc
Jade Kim <csia19030@gmail.com>
2025-04-08 16:53:23 +0900

    style: change style of code according to prettier guidelines

--- src/controllers/communityController.ts
+++ src/controllers/communityController.ts
@@ -4,90 +4,90 @@ import { UserInfo } from "../services/authService";
 import communityService from "../services/communityService";
 
 export async function createPost(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<void> {
-    try {
-        const { title, content } = req.body as { title: string; content: string };
-        const userId = (req as Request & { user: UserInfo }).user.userId;
+  try {
+    const { title, content } = req.body as { title: string; content: string };
+    const userId = (req as Request & { user: UserInfo }).user.userId;
 
-        const newPost = await communityService.createPost(userId, title, content);
+    const newPost = await communityService.createPost(userId, title, content);
 
-        res.status(StatusCodes.CREATED).json(newPost);
-
-    } catch (error) {
-        next(error);
-    }
+    res.status(StatusCodes.CREATED).json(newPost);
+  } catch (error) {
+    next(error);
+  }
 }
 
 export async function getPostDetail(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<void> {
-    try {
-        const {postId} = req.params;
+  try {
+    const { postId } = req.params;
 
-        const postDetail = await communityService.getPostDetail(parseInt(postId));
+    const postDetail = await communityService.getPostDetail(parseInt(postId));
 
-        if (!postDetail) {
-            res.status(StatusCodes.NOT_FOUND).json({ message: "Post not found" });
-            return;
-        }
-
-        res.status(StatusCodes.OK).json(postDetail);
-
-    } catch (error) {
-        next(error);
+    if (!postDetail) {
+      res.status(StatusCodes.NOT_FOUND).json({ message: "Post not found" });
+      return;
     }
+
+    res.status(StatusCodes.OK).json(postDetail);
+  } catch (error) {
+    next(error);
+  }
 }
 
 export async function getPosts(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<void> {
-    try {
-        const posts = await communityService.getPosts();
+  try {
+    const posts = await communityService.getPosts();
 
-        res.status(StatusCodes.OK).json(posts);
-
-    } catch (error) {
-        next(error);
-    }
+    res.status(StatusCodes.OK).json(posts);
+  } catch (error) {
+    next(error);
+  }
 }
 
 export async function deletePost(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<void> {
-    try {
-        const { postId } = req.params;
+  try {
+    const { postId } = req.params;
 
-        await communityService.deletePost(parseInt(postId));
+    await communityService.deletePost(parseInt(postId));
 
-        res.status(StatusCodes.NO_CONTENT);
-    } catch (error) {
-        next(error);
-    }
+    res.status(StatusCodes.NO_CONTENT);
+  } catch (error) {
+    next(error);
+  }
 }
 
 export async function updatePost(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<void> {
-    try {
-        const postId = req.params.postId;
-        const { title, content } = req.body as { title: string; content: string };
+  try {
+    const postId = req.params.postId;
+    const { title, content } = req.body as { title: string; content: string };
 
-        const updatedPost = await communityService.updatePost(parseInt(postId), title, content);
+    const updatedPost = await communityService.updatePost(
+      parseInt(postId),
+      title,
+      content
+    );
 
-        res.status(StatusCodes.CREATED).json(updatedPost);
-
-    } catch(error) {
-        next(error);
-    }
+    res.status(StatusCodes.CREATED).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
 }
--- src/services/communityService.ts
+++ src/services/communityService.ts
@@ -2,68 +2,68 @@ import dbDayjs from "../lib/dayjs.js";
 import prisma from "../lib/prisma.js";
 
 const communityService = {
-    createPost,
-    getPostDetail,
-    getPosts,
-    deletePost,
-    updatePost,
+  createPost,
+  getPostDetail,
+  getPosts,
+  deletePost,
+  updatePost,
 };
 
 export default communityService;
 
 export async function createPost(
-    userId: number,
-    title: string,
-    content: string,
-){
-    return await prisma.communityPost.create({
-        data: {
-            userId,
-            title,
-            content,
-            createdAt: dbDayjs(),
-            updatedAt: dbDayjs(),
-        }
-    });
+  userId: number,
+  title: string,
+  content: string
+) {
+  return await prisma.communityPost.create({
+    data: {
+      userId,
+      title,
+      content,
+      createdAt: dbDayjs(),
+      updatedAt: dbDayjs(),
+    },
+  });
 }
 
 export async function getPostDetail(postId: number) {
-    return await prisma.communityPost.findUnique({
-        where: {id: postId}
-    });
+  return await prisma.communityPost.findUnique({
+    where: { id: postId },
+  });
 }
 
 export async function getPosts() {
-    return await prisma.communityPost.findMany({
-        orderBy: {createdAt: "desc"},
-        include: {
-            user: {
-                select: {
-                    id: true,
-                    nickname: true,
-                }
-            }
-        }
-    });
+  return await prisma.communityPost.findMany({
+    orderBy: { createdAt: "desc" },
+    include: {
+      user: {
+        select: {
+          id: true,
+          nickname: true,
+        },
+      },
+    },
+  });
 }
 
 export async function deletePost(postId: number) {
-    return await prisma.communityPost.delete({
-        where: {id: postId}
-    });
+  return await prisma.communityPost.delete({
+    where: { id: postId },
+  });
 }
 
 export async function updatePost(
-    postId: number,
-    title: string,
-    content: string,
+  postId: number,
+  title: string,
+  content: string
 ) {
-    return await prisma.communityPost.update({
-        where: {id: postId},
-        data: {
-            title,
-            content,
-            updatedAt: dbDayjs(),
-        }
-    });
+  return await prisma.communityPost.update({
+    where: { id: postId },
+    data: {
+      title,
+      content,
+      updatedAt: dbDayjs(),
+    },
+  });
 }
