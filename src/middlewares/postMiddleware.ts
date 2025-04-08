aece4acb76e5ee864df5cdfd5e6f7d980cff6cb0
Jade Kim <csia19030@gmail.com>
2025-04-08 16:51:25 +0900

    feat: add middleware for checking ownership of post

---
+++ src/middlewares/postMiddleware.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import { UserInfo } from "../services/authService";
+import { StatusCodes } from "http-status-codes";
+import prisma from "../lib/prisma";
+
+const postMiddleware = {
+    checkPostOwnership
+};
+
+export default postMiddleware;
+
+async function checkPostOwnership (
+    req: Request & { user?: UserInfo },
+    res: Response,
+    next: NextFunction
+): Promise<void> {
+    const { postId } = req.params;
+    const user = req.user as UserInfo;
+    const userId = user.userId;
+
+    try {
+        const post = await prisma.communityPost.findUnique({
+            where: { id: parseInt(postId) },
+            select: { userId: true },
+        });
+
+        if(!post) {
+            res.status(StatusCodes.NOT_FOUND).json({ message: "조건에 해당하는 게시물이 존재하지 않습니다." });
+            return;
+        }
+
+        if(post.userId !== userId) {
+            res.status(StatusCodes.FORBIDDEN).json({ message: "다른 사용자의 게시물은 접근 불가합니다." });
+            return;
+        }
+    } catch (error) {
+        next(error);
+    }
+}
