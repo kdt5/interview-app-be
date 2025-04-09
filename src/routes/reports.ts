82611125b3ff1605755d1b97867a224d0e1c25b7
Jade Kim <csia19030@gmail.com>
2025-04-09 13:20:51 +0900

    feat: add validation for reports api

--- src/routes/reports.ts
+++ src/routes/reports.ts
@@ -1,13 +1,58 @@
 import { Router } from "express";
-import { createReport, deleteReport, getReportDetail, getReports, updateReport } from "../controllers/reportController";
+import {
+  createReport,
+  deleteReport,
+  getReportDetail,
+  getReports,
+  updateReport,
+} from "../controllers/reportController";
 import authMiddleware from "../middlewares/authMiddleware";
+import {
+  validateCreateReportBody,
+  validateGetReportQuery,
+  validateReportId,
+  validateUpdateReportBody,
+} from "../middlewares/reportValidator";
+import { validationErrorMiddleware } from "../middlewares/validationErrorMiddleware";
 
 const router = Router();
 
-router.get("/", getReports);
-router.get("/:reportId", getReportDetail);
-router.post("/", authMiddleware.authenticate, createReport);
-router.delete("/:reportId", deleteReport);
-router.patch("/:reportId", updateReport);
+// 사용자 권한
+router.post(
+  "/",
+  authMiddleware.authenticate,
+  validateCreateReportBody,
+  validationErrorMiddleware,
+  createReport
+);
+
+// 어드민 권한
+router.get(
+  "/",
+  authMiddleware.authenticate,
+  validateGetReportQuery,
+  validationErrorMiddleware,
+  getReports
+);
+router.get(
+  "/:reportId",
+  authMiddleware.authenticate,
+  validateReportId,
+  validationErrorMiddleware,
+  getReportDetail
+);
+router.delete(
+  "/:reportId",
+  validateReportId,
+  validationErrorMiddleware,
+  deleteReport
+);
+router.patch(
+  "/:reportId",
+  validateReportId,
+  validateUpdateReportBody,
+  validationErrorMiddleware,
+  updateReport
+);
 
 export default router;
