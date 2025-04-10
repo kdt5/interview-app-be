25b3fb3b68f28bb83f6a6e886bcb9ed4afbaa8c4
Jade Kim <119997396+poppinpigeon@users.noreply.github.com>
2025-04-10 16:56:45 +0900

    Feature/reports (#50)
    게시글/댓글 신고 관련 API 추가
    - GET /reports: 신고 전체 내역 조회
    - GET /reports/:reportId: 신고 내역 상세 조회
    - POST /reports: 신고 내역 생성
    - PATCH /reports/:reportId: 신고 내역 처리상황 수정
    - DELETE /reports/:reportId: 신고 내역 삭제
    
    POST 외의 api는 admin 계정으로 로그인 한 경우에만 사용할 수 있도록 추후 작업 필요합니다.

--- src/services/reportService.ts
+++ src/services/reportService.ts
@@ -1,51 +1,61 @@
 import { ReportStatus, ReportTargetType } from "@prisma/client";
 import prisma from "../lib/prisma";
+import { GetReportRequest } from "../controllers/reportController";
 
 const reportService = {
-    getReports,
-    getReportDetail,
-    createReport,
-    deleteReport,
-    updateReport
-}
+  getReports,
+  getReportDetail,
+  createReport,
+  deleteReport,
+  updateReport,
+};
 
 export default reportService;
 
-async function getReports() {
-    return prisma.report.findMany();
+async function getReports(filters: GetReportRequest["query"]) {
+  const { targetType, status, targetId, reporterId } = filters;
+
+  return prisma.report.findMany({
+    where: {
+      ...(targetType && { targetType }),
+      ...(status && { status }),
+      ...(targetId && { targetId: Number(targetId) }),
+      ...(reporterId && { reporterId: Number(reporterId) }),
+    },
+  });
 }
 
 async function getReportDetail(reportId: number) {
-    return prisma.report.findUnique({
-        where: {id: reportId}
-    });
+  return prisma.report.findUnique({
+    where: { id: reportId },
+  });
 }
 
 async function createReport(
-    reporterId: number,
-    targetType: ReportTargetType,
-    targetId: number,
-    reason: string
+  reporterId: number,
+  targetType: ReportTargetType,
+  targetId: number,
+  reason: string
 ) {
-    return prisma.report.create({
-        data: {
-            reporterId,
-            targetType,
-            targetId,
-            reason
-        }
-    });
+  return prisma.report.create({
+    data: {
+      reporterId,
+      targetType,
+      targetId,
+      reason,
+    },
+  });
 }
 
 async function deleteReport(reportId: number) {
-    return prisma.report.delete({
-        where: { id: reportId }
-    });
+  return prisma.report.delete({
+    where: { id: reportId },
+  });
 }
 
 async function updateReport(reportId: number, status: ReportStatus) {
-    return prisma.report.update({
-        where: { id: reportId },
-        data: { status }
-    });
+  return prisma.report.update({
+    where: { id: reportId },
+    data: { status },
+  });
 }
