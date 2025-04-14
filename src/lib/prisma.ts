2b7251da927e033c559ef68b0d292028ea3cc452
FeelsBotMan <sjb0229@gmail.com>
2025-04-14 12:25:51 +0900

    feature: 즐겨찾기 생성 및 삭제 기능에 오류 처리 추가

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -7,31 +7,41 @@ const prisma = basePrisma.$extends({
   query: {
     favorite: {
       async create({ args, query }) {
-        return basePrisma.$transaction(async (tx) => {
-          const result = (await query({ ...args, tx })) as Favorite;
-          await rankingService.incrementFavoriteCount(
-            result.targetType as FavoriteTargetType,
-            Number(result.targetId),
-            tx
-          );
-          return result;
-        });
-      },
-      async delete({ args, query }) {
-        return basePrisma.$transaction(async (tx) => {
-          const favorite = await tx.favorite.findUnique({
-            where: args.where,
-          });
-          const result = await query({ ...args, tx });
-          if (favorite) {
-            await rankingService.decrementFavoriteCount(
-              favorite.targetType as FavoriteTargetType,
-              Number(favorite.targetId),
+        try {
+          return basePrisma.$transaction(async (tx) => {
+            const result = (await query({ ...args, tx })) as Favorite;
+            await rankingService.incrementFavoriteCount(
+              result.targetType as FavoriteTargetType,
+              Number(result.targetId),
               tx
             );
-          }
-          return result;
-        });
+            return result;
+          });
+        } catch (error) {
+          console.error(error);
+          throw error;
+        }
+      },
+      async delete({ args, query }) {
+        try {
+          return basePrisma.$transaction(async (tx) => {
+            const favorite = await tx.favorite.findUnique({
+              where: args.where,
+            });
+            const result = await query({ ...args, tx });
+            if (favorite) {
+              await rankingService.decrementFavoriteCount(
+                favorite.targetType as FavoriteTargetType,
+                Number(favorite.targetId),
+                tx
+              );
+            }
+            return result;
+          });
+        } catch (error) {
+          console.error(error);
+          throw error;
+        }
       },
     },
   },
