47a98266b1bcdc12bbe6101d57494d2aff03687a
Jade Kim <csia19030@gmail.com>
2025-04-16 16:36:00 +0900

    chore: add categoryId validation to validator middleware

--- src/middlewares/postValidator.ts
+++ src/middlewares/postValidator.ts
@@ -19,6 +19,11 @@ export const validatePostBody = [
     .withMessage("내용은 필수입니다.")
     .isString()
     .withMessage("내용은 문자열이어야 합니다."),
+  body("categoryId")
+    .exists()
+    .withMessage("게시물 카테고리 아이디는 필수입니다.")
+    .isInt({ min: 1 })
+    .withMessage("게시물 카테고리 아이디는 1 이상의 정수만 가능합니다."),
 ];
 
 export const validatePostQuery = [
