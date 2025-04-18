8766c2f763a8fd7c0263f9c2583a871eb7077b44
Jade Kim <csia19030@gmail.com>
2025-04-18 15:52:19 +0900

    feat: implement update profile image feature

---
+++ src/middlewares/uploadMiddleware.ts
@@ -0,0 +1,43 @@
+import { DeleteObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import prisma from "../lib/prisma";
+
+const uploadMiddleware = {
+    deleteImageFromS3,
+    getUserImageUrl,
+};
+
+export default uploadMiddleware;
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+async function deleteImageFromS3(fileUrl: string) {
+    //remove from s3
+  const bucket = process.env.AWS_S3_BUCKET_NAME!;
+  const key = new URL(fileUrl).pathname.slice(1);
+
+  const command = new DeleteObjectCommand({
+    Bucket: bucket,
+    Key: key,
+  });
+
+  await s3.send(command);
+}
+
+async function getUserImageUrl(userId: number): Promise<string> {
+    const user = await prisma.user.findUnique({
+        where: { id: userId },
+        select: { profileImageUrl: true }
+    });
+
+    if (!user || !user.profileImageUrl) {
+        return "";
+    } else {
+        return user.profileImageUrl as string;
+    }
+}
--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -1,4 +1,5 @@
 import prisma from "../lib/prisma.js";
+import uploadMiddleware from "../middlewares/uploadMiddleware.js";
 
 const uploadService = {
     uploadImage,
@@ -7,6 +8,12 @@ const uploadService = {
 export default uploadService;
 
 async function uploadImage(userId: number, imageUrl: string) {
+    const existingImageUrl = await uploadMiddleware.getUserImageUrl(userId);
+
+    if (existingImageUrl !== "") {
+        await uploadMiddleware.deleteImageFromS3(existingImageUrl);
+    }
+
     return await prisma.user.update({
         where: { id: userId },
         data: { profileImageUrl: imageUrl },
