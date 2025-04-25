cdcd304a20ea05bfb1eaf2b8fd1ed8ea571f8923
asdfbn12345 <asdfbn12345@gmail.com>
2025-04-25 16:39:39 +0900

    feature: Update getCategory optional query positionId

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -8,31 +8,26 @@ export interface CategoryQueryRequest extends Request {
   };
 }
 
-export async function getAllCategories(
+export async function getCategories(
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> {
   try {
     const request = req as CategoryQueryRequest;
-    const positionId = request.query.positionId;
-    if (positionId) {
-      const categories = await categoryService.getPositionCategories(
-        parseInt(positionId)
-      );
-      if (categories.length === 0) {
-        res
-          .status(StatusCodes.NOT_FOUND)
-          .json({ message: "조건에 해당하는 카테고리가 존재하지 않습니다." });
-        return;
-      }
+    const positionId = request.query.positionId
+      ? parseInt(request.query.positionId)
+      : undefined;
+    const categories = await categoryService.getCategories(positionId);
 
-      res.status(StatusCodes.OK).json(categories);
-    } else {
+    if (categories.length === 0) {
       res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ message: "positionId 파라미터가 필요합니다." });
+        .status(StatusCodes.NOT_FOUND)
+        .json({ message: "조건에 해당하는 카테고리가 존재하지 않습니다." });
+      return;
     }
+
+    res.status(StatusCodes.OK).json(categories);
   } catch (error) {
     console.error(error);
     next(error);
--- src/middlewares/categoriesValidator.ts
+++ src/middlewares/categoriesValidator.ts
@@ -2,15 +2,21 @@ import { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 import { CategoryQueryRequest } from "../controllers/categoriesController.js";
 
-export function validateGetAllCategoriesQuery(
+export function validateGetCategories(
   req: Request,
   res: Response,
   next: NextFunction
 ): void {
   const request = req as CategoryQueryRequest;
-  const positionId = request.query.positionId;
 
-  if (positionId && isNaN(parseInt(positionId))) {
+  if (request.query.positionId === undefined) {
+    next();
+    return;
+  }
+
+  const positionId = parseInt(request.query.positionId);
+
+  if (isNaN(positionId)) {
     res
       .status(StatusCodes.BAD_REQUEST)
       .json({ message: "positionId는 숫자여야 합니다." });
