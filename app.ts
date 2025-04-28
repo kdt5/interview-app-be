a45a1c44e93e8c834e5539d1bcc104cfb63ecb77
FeelsBotMan <sjb0229@gmail.com>
2025-04-28 18:44:08 +0900

    CORS의 origin 설정을 배열에서 단일 URL로 변경

--- app.ts
+++ app.ts
@@ -24,9 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL
-      ? process.env.FRONTEND_URL.split(",").map((url) => url.trim())
-      : [],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
