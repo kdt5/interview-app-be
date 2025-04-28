d282b02ef8a55e669c9f990f5d3dfd8eecf986dd
FeelsBotMan <sjb0229@gmail.com>
2025-04-28 18:12:46 +0900

    CORS의 origin을 배열로 처리하도록 수정

--- app.ts
+++ app.ts
@@ -24,7 +24,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL
+      ? process.env.FRONTEND_URL.split(",").map((url) => url.trim())
+      : [],
     credentials: true,
   })
 );
